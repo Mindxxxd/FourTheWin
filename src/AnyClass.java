8983d932898630d3fcf8c5d920fbc510c97c9c37
Mindxxxd <25640361+Mindxxxd@users.noreply.github.com>
2020-02-10 13:34:31 +0100

    Initial commit

---
+++ src/AnyClass.java
@@ -0,0 +1,17 @@
+/**This is a test class for github.
+ * @author Mindxxxd.
+ * 10.02.2020.
+ * 
+ */
+public class AnyClass {
+
+	/**COMMENT
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// CODE
+		// This is not the code you are looking for.
+
+	}
+
+}
