8327b786094ab2a42904921bc8edd5af15590890
Mindxxxd <25640361+Mindxxxd@users.noreply.github.com>
2020-02-17 21:03:32 +0100

    Uptaded Structure and added a test window

---
+++ src/testify/TestWindow.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package testify;
+
+import javax.swing.JFrame;
+
+/**COMMENT
+ * @author Mindxxxd.
+ * 14.02.2020.
+ * 
+ */
+public class TestWindow  extends JFrame {
+
+	/**COMMENT
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// CODE
+
+	}
+
+}
