be2e7827362c50e19e3835c9c2034b22127e5046
Mindxxxd <25640361+Mindxxxd@users.noreply.github.com>
2020-02-19 14:19:44 +0100

    reorganisation

---
+++ src/data/DataModell.java
@@ -0,0 +1,38 @@
+package data;
+
+import views.View;
+
+/**
+ * @author Mindxxxd.
+ * 19.02.2020.
+ * 
+ */
+public class DataModell {
+	//This is a placeholder class.
+	
+	public void addView(View view) {
+		//placeholder
+	}
+	
+	public boolean makeTurn() {
+		//Placeholder
+		return true;
+	}
+	
+	/**COMMENT
+	 * @param direction if -1, try left, if 1 try right, if 0, do nnothing.
+	 * @return
+	 */
+	public boolean movePlayStone(int direction) {
+		//Placeholder
+		return true;
+	}
+	/**COMMENT
+	 * 
+	 */
+	private void gameFinished(Player winner) {
+		//CODE Update Views 
+
+	}
+
+}
---
+++ src/data/Player.java
@@ -0,0 +1,10 @@
+package data;
+
+/**This is the enum of Players.
+ * @author Mindxxxd.
+ * 19.02.2020.
+ * 
+ */
+public enum Player {
+	YELLOW, RED, NOONE;
+}
